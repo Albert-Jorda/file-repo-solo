8bf9dff58b98d38b76731680cb34518327fdf9be
YourAlly <Royettemiranda@YAHOO.COM>
2022-01-20 19:12:24 +0800

    Registered models on admin

--- repo/admin.py
+++ repo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from models import File, Folder, HeirData
 # Register your models here.
+admin.site.register(File)
+admin.site.register(Folder)
+admin.site.register(HeirData)
