cd8f7338bbb319d2c43476707567bf2f1defea34
YourAlly <Royettemiranda@YAHOO.COM>
2022-01-20 18:03:41 +0800

    Auth feature initial commit

--- repo/views.py
+++ repo/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def index(request):
+    # TODO
+    pass
+
+def login(request):
+    if request.method == "POST":
+        # TODO
+        pass
+    else:
+        # TODO
+        pass
+
+def register(request):
+    if request.method == "POST":
+        # TODO
+        pass
+    else:
+        # TODO
+        pass
+
+def logout(request):
+    # TODO
+    pass
