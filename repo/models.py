4d798d0cb156646225965b2907f7a5210dd84509
YourAlly <Royettemiranda@YAHOO.COM>
2022-01-20 17:51:45 +0800

    Added comments to models.py

--- repo/models.py
+++ repo/models.py
@@ -12,14 +12,20 @@ from django.core.exceptions import ValidationError
 class User(AbstractUser):
     pass
 
-
+'''
+Each user will have a default Folder with no name, and has the is_root value of true
+'''
 class Folder(models.Model):
     owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="folders")
     name = models.CharField(max_length=64)
     is_root =  models.BooleanField(default=False)
     is_shared =  models.BooleanField(default=False)
 
+'''
+A folder, if not is_root should have an heir data with it as the "folder"
+It will relate itself to a folder which it will refer to as its "parent"
 
+'''
 class HeirData(models.Model):
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="parents")
     parent = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="children")
@@ -27,4 +33,5 @@ class HeirData(models.Model):
 
 class File(models.Model):
     name = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(default=timezone.now)
     folder = models.ForeignKey(Folder, on_delete=models.CASCADE, related_name="files")
