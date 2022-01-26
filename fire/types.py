f276f75c29b29ee22839705a2af3f99614ece26a
YourAlly <Royettemiranda@YAHOO.COM>
2022-01-26 22:58:53 +0800

    Initial commit for file category feature

---
+++ fire/types.py
@@ -0,0 +1,17 @@
+types = {
+    "video": [
+        "mp4", "mkv", "webm"
+    ],
+
+    "text": [
+        "txt", "py", "js"
+    ],
+
+    "image": [
+        "jpg", "jpeg", "png"
+    ],
+
+    "executable":[
+        "exe"
+    ]
+}
