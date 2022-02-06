af1cf604a8ec584d668fb0d51637945709373cbd
Kimberly Soriano <kimberlysoriano144@gmail.com>
2022-02-06 15:59:46 +0800

    open file onclick file name

--- repo/static/repo/script.js
+++ repo/static/repo/script.js
@@ -1,5 +1,5 @@
 table_row_folder = document.querySelectorAll('tr.folder');
-table_row_file = document.querySelectorAll('tr.file');
+td_file = document.querySelectorAll(".file_open");
 drpDwn_categ = document.querySelector('#categories');
 drpDwn_orderBy = document.querySelector('#order_by');
 drpDwn_sequence = document.querySelector('#sequence');
@@ -11,12 +11,13 @@ table_row_folder.forEach(trf => {
     });
 });
 
-table_row_file.forEach(trf => {
-    trf.addEventListener("click", () => {
-        let url = trf.getAttribute("data-url");
+td_file.forEach(tdf => {
+    tdf.addEventListener("click", () => {
+        let url = tdf.getAttribute("data-url");
         window.open(url, "_blank").focus();
     });
 });
+
 
 drpDwn_categ.addEventListener("change", () => {
     url = drpDwn_categ.options[drpDwn_categ.selectedIndex].getAttribute('data-url');
